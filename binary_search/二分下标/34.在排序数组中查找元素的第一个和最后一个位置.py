eccd1a5dc3870e6ca319377682c48071ece7ec15
cbqin <cbbupt@163.com>
2020-12-10 15:17:36 +0800

    update binary search

---
+++ binary_search/二分下标/34.在排序数组中查找元素的第一个和最后一个位置.py
@@ -0,0 +1,102 @@
+#
+# @lc app=leetcode.cn id=34 lang=python3
+#
+# [34] 在排序数组中查找元素的第一个和最后一个位置
+#
+# https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+#
+# algorithms
+# Medium (41.90%)
+# Likes:    765
+# Dislikes: 0
+# Total Accepted:    187.9K
+# Total Submissions: 447.7K
+# Testcase Example:  '[5,7,7,8,8,10]\n8'
+#
+# 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+#
+# 如果数组中不存在目标值 target，返回 [-1, -1]。
+#
+# 进阶：
+#
+#
+# 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+#
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：nums = [5,7,7,8,8,10], target = 8
+# 输出：[3,4]
+#
+# 示例 2：
+#
+#
+# 输入：nums = [5,7,7,8,8,10], target = 6
+# 输出：[-1,-1]
+#
+# 示例 3：
+#
+#
+# 输入：nums = [], target = 0
+# 输出：[-1,-1]
+#
+#
+#
+# 提示：
+#
+#
+# 0
+# -10^9 
+# nums 是一个非递减数组
+# -10^9 
+#
+#
+#
+
+# @lc code=start
+
+
+class Solution:
+    def searchFirstPosition(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left = 0
+        right = len(nums)-1
+        while left < right:
+            mid = left+(right-left)//2
+            if nums[mid] < target:
+                left = mid+1
+            else:
+                right = mid
+        if nums[left] == target:
+            return left
+        return -1
+
+    def searchLastPosition(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left = 0
+        right = len(nums)-1
+        while left < right:
+            mid = left+(right-left+1)//2
+            if nums[mid] > target:
+                right = mid-1
+            else:
+                left = mid
+        if nums[left] == target:
+            return left
+        return -1
+
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        first_position = self.searchFirstPosition(nums, target)
+
+        if first_position == -1:
+            return [-1, -1]
+
+        last_position = self.searchLastPosition(nums, target)
+
+        return [first_position, last_position]
+# @lc code=end
