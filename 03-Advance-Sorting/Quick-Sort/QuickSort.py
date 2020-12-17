9b387dfd7bcaacc63e73d6c4c3ca3f6ec40a6e58
cbqin <cbqin0323@gmail.com>
2020-12-17 10:02:20 +0800

    add quick sort

---
+++ 03-Advance-Sorting/Quick-Sort/QuickSort.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def sort(self, nums: List[int]) -> List[int]:
+        pass
+
+    def quickSort1(self, nums: List[int]) -> None:
+        pass
+
+    def quickSort2(self, nums: List[int]) -> None:
+        pass
+
+    def quickSort3(self, nums: List[int]) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    pass
