c8b03ae487b653350ac900e55ec01e56ce297163
cbqin <cbqin0323@gmail.com>
2020-12-19 16:56:06 +0800

    update counting sort

---
+++ 03-Advance-Sorting/Counting-Sort/Counting_Sort.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class CountingSort:
+    def countingSort(self, nums):
+        n = len(nums)
+        if n == 0:
+            return nums
+        max_value = 0
+        for num in nums:
+            assert num >= 0
+            if num >= max_value:
+                max_value = num
+        count = [0]*(max_value+1)
+
+        for num in nums:
+            count[num] += 1
+
+        index = 0
+        for i, c in enumerate(count):
+            for j in range(c):
+                nums[index+j] = i
+            index += c
+        return nums
+
+
+if __name__ == "__main__":
+    nums = [2, 5, 3, 0, 2, 3, 0, 3]
+    cs = CountingSort()
+    nums = cs.countingSort(nums)
+    print(nums)
