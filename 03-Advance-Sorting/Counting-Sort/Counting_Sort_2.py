4cb1ddd43e8958ff7d9256095a40b28eebd55120
cbqin <cbqin0323@gmail.com>
2020-12-19 17:15:44 +0800

    update counting sort

--- 03-Advance-Sorting/Counting-Sort/Counting_Sort_2.py
+++ 03-Advance-Sorting/Counting-Sort/Counting_Sort_2.py
@@ -1,26 +1,32 @@
+import copy
 from typing import List
 
 
 class CountingSort:
     def countingSort(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        if n == 0:
+        if n < 2:
             return nums
         max_value = 0
         for num in nums:
             assert num >= 0
             if num >= max_value:
                 max_value = num
-        count = [0]*(max_value+1)
 
+        count = [0]*(max_value+1)
         for num in nums:
             count[num] += 1
 
-        index = 0
-        for i, c in enumerate(count):
-            for j in range(c):
-                nums[index+j] = i
-            index += c
+        for i in range(1, max_value+1):
+            count[i] += count[i-1]
+
+        nums_copy = copy.deepcopy(nums)
+
+        for i in range(n-1, -1, -1):
+            position = count[nums_copy[i]]-1
+            nums[position] = nums_copy[i]
+            count[nums_copy[i]] -= 1
+
         return nums
 
 
