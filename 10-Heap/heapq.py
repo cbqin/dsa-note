ed74c4e777083da1639c456b75533604f92bc8ed
cbqin <cbqin0323@gmail.com>
2021-01-02 17:37:53 +0800

    update queue

---
+++ 10-Heap/heapq.py
@@ -0,0 +1,14 @@
+"""Heap queue algorithm (a.k.a. priority queue).
+Heaps are arrays for which a[k] <= a[2*k+1] and a[k] <= a[2*k+2] for
+all k, counting elements from 0.  For the sake of comparison,
+non-existing elements are considered to be infinite.  The interesting
+property of a heap is that a[0] is always its smallest element.
+Usage:
+heap = []            # creates an empty heap
+heappush(heap, item) # pushes a new item on the heap
+item = heappop(heap) # pops the smallest item from the heap
+item = heap[0]       # smallest item on the heap without popping it
+heapify(x)           # transforms list into a heap, in-place, in linear time
+item = heapreplace(heap, item) # pops and returns smallest item, and adds
+                               # new item; the heap size is unchanged
+"""
