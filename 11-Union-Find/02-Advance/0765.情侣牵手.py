3052f181b1b6eca988e677faebaccdbeae7ef758
cbqin <cbqin0323@gmail.com>
2021-01-08 14:26:02 +0800

    update union find

--- 11-Union-Find/02-Advance/0765.情侣牵手.py
+++ 11-Union-Find/02-Advance/0765.情侣牵手.py
@@ -52,22 +52,75 @@ from typing import List
 
 
 class Solution:
+    # def minSwapsCouples(self, row: List[int]) -> int:
+    #     ans = 0
+    #     position = {}
+    #     for i, num in enumerate(row):
+    #         position[num] = i
+
+    #     for i in range(0, len(row), 2):
+    #         x = row[i]
+    #         if row[i+1] == x ^ 1:
+    #             continue
+    #         ans += 1
+    #         j = position[x ^ 1]
+    #         row[i+1], row[j] = row[j], row[i+1]
+    #         position[row[i+1]] = i+1
+    #         position[row[j]] = j
+
+    #     return ans
+
     def minSwapsCouples(self, row: List[int]) -> int:
-        ans = 0
-        position = {}
-        for i, num in enumerate(row):
-            position[num] = i
+        n = len(row)//2
+        uf = UnionFind(n)
+        for i in range(n):
+            uf.union(row[2*i]//2, row[2*i+1]//2)
+        return n-uf.getCount()
 
-        for i in range(0, len(row), 2):
-            x = row[i]
-            if row[i+1] == x ^ 1:
-                continue
-            ans += 1
-            j = position[x ^ 1]
-            row[i+1], row[j] = row[j], row[i+1]
-            position[row[i+1]] = i+1
-            position[row[j]] = j
 
-        return ans
+class UnionFind(object):
+    def __init__(self, N: int):
+        self.N = N
+        self.count = N
+        self.parents = [0]*N
+        self.rank = [1]*N
+
+        for i in range(N):
+            self.parents[i] = i
+
+    # 路径压缩：隔代压缩
+    def find(self, x: int) -> int:
+        while x != self.parents[x]:
+            self.parents[x] = self.parents[self.parents[x]]
+            x = self.parents[x]
+        return x
+
+    # 路径压缩：完全压缩
+    # def find(self, x: int) -> int:
+    #     if x != self.parents[x]:
+    #         self.parents[x] = self.find(self.parents[x])
+    #     return self.parents[x]
+
+    # O(mα(n))
+    def union(self, x: int, y: int) -> None:
+        rootx = self.find(x)
+        rooty = self.find(y)
+
+        if rootx == rooty:
+            return
+
+        if self.rank[rootx] == self.rank[rooty]:
+            self.parents[rootx] = rooty
+            self.rank[rooty] += 1
+        elif self.rank[rootx] < self.rank[rooty]:
+            self.parents[rootx] = rooty
+        else:
+            self.parents[rooty] = rootx
+
+        self.count -= 1
+
+    def getCount(self) -> int:
+        return self.count
+
 
 # @lc code=end
