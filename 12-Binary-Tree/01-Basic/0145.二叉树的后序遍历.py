a2d548ab893f24f125f088a6cdad15598e17f43b
cbqin <cbqin0323@gmail.com>
2021-01-11 11:14:58 +0800

    update binary tree

--- 12-Binary-Tree/01-Basic/0145.二叉树的后序遍历.py
+++ 12-Binary-Tree/01-Basic/0145.二叉树的后序遍历.py
@@ -58,6 +58,25 @@ class Solution:
     #     return res
 
     def postorderTraversal(self, root: TreeNode) -> List[int]:
-        pass
+        res = []
+        if not root:
+            return res
+
+        stack = []
+        prev = None
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if not root.right or root.right == prev:
+                res.append(root.val)
+                prev = root
+                root = None
+            else:
+                stack.append(root)
+                root = root.right
+
+        return res
 
 # @lc code=end
