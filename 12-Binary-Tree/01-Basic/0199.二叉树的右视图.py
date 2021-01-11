287335d1d11e3f8b4a0803eedd6b27a71bc17add
cbqin <cbqin0323@gmail.com>
2021-01-11 19:36:28 +0800

    update binary tree

---
+++ 12-Binary-Tree/01-Basic/0199.二叉树的右视图.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode.cn id=199 lang=python3
+#
+# [199] 二叉树的右视图
+#
+# https://leetcode-cn.com/problems/binary-tree-right-side-view/description/
+#
+# algorithms
+# Medium (64.62%)
+# Likes:    387
+# Dislikes: 0
+# Total Accepted:    80.5K
+# Total Submissions: 124.3K
+# Testcase Example:  '[1,2,3,null,5,null,4]'
+#
+# 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+#
+# 示例:
+#
+# 输入: [1,2,3,null,5,null,4]
+# 输出: [1, 3, 4]
+# 解释:
+#
+# ⁠  1            <---
+# ⁠/   \
+# 2     3         <---
+# ⁠\     \
+# ⁠ 5     4       <---
+#
+#
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+
+from typing import List
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        res = []
+        queue = deque([root])
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                if i == size-1:
+                    res.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return res
+# @lc code=end
