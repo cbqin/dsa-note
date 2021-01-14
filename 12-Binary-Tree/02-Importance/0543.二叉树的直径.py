2aad63fa6ace96198098ae29952d0a8787ad0b2c
cbqin <cbqin0323@gmail.com>
2021-01-14 17:40:03 +0800

    update binary tree

---
+++ 12-Binary-Tree/02-Importance/0543.二叉树的直径.py
@@ -0,0 +1,62 @@
+#
+# @lc app=leetcode.cn id=543 lang=python3
+#
+# [543] 二叉树的直径
+#
+# https://leetcode-cn.com/problems/diameter-of-binary-tree/description/
+#
+# algorithms
+# Easy (51.99%)
+# Likes:    588
+# Dislikes: 0
+# Total Accepted:    87.7K
+# Total Submissions: 168.4K
+# Testcase Example:  '[1,2,3,4,5]'
+#
+# 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+#
+#
+#
+# 示例 :
+# 给定二叉树
+#
+# ⁠         1
+# ⁠        / \
+# ⁠       2   3
+# ⁠      / \
+# ⁠     4   5
+#
+#
+# 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+#
+#
+#
+# 注意：两结点之间的路径长度是以它们之间边的数目表示。
+#
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def __init__(self):
+        self.max_diameter = 1
+
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        def dfs(root):
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            self.max_diameter = max(self.max_diameter, left+right+1)
+            return max(left, right)+1
+        dfs(root)
+        return self.max_diameter-1
+
+# @lc code=end
