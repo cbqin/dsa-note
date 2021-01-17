36b90794006aaa2a18208d393b9cbdfa1231fbd2
cbqin <cbqin0323@gmail.com>
2021-01-17 16:00:13 +0800

    update bst

--- 13-Binary-Search-Tree/binary_search_tree.py
+++ 13-Binary-Search-Tree/binary_search_tree.py
@@ -26,7 +26,7 @@ class BinarySearchTree(object):
         return self.size == 0
 
     def insert(self, key: int, value: int) -> None:
-        self._insert(self.root, key, value)
+        self.root = self._insert(self.root, key, value)
 
     def _insert(self, node: TreeNode, key: int, value: int) -> TreeNode:
         if node is None:
@@ -69,3 +69,60 @@ class BinarySearchTree(object):
             return self._contain(node.left, key)
         else:
             return self._contain(node.right, key)
+
+    def remove(self, key: int) -> None:
+        self.root = self._remove(self.root, key)
+
+    def _remove(self, node: TreeNode, key: int) -> TreeNode:
+        if node is None:
+            return None
+
+        if key < node.key:
+            node.left = self._remove(node.left, key)
+        elif key > node.key:
+            node.right = self._remove(node.right, key)
+        else:
+            if node.left is None:
+                right = node.right
+                node.right = None
+                del node
+                self.size -= 1
+                return right
+
+            if node.right is None:
+                left = node.left
+                node.left = None
+                del node
+                self.size -= 1
+                return left
+
+            successor = self.minimum(node.right)
+            self.size += 1
+            successor.right = self.removeMinimum(node.right)
+            successor.left = node.left
+            node.left = None
+            node.right = None
+            del node
+            self.size -= 1
+            return successor
+
+    def minimum(self, node: TreeNode) -> TreeNode:
+        if node.left is None:
+            return node
+
+        return self.minimum(node.left)
+
+    def removeMinimum(self, node: TreeNode) -> TreeNode:
+        if node.left is None:
+            right = node.right
+            node.right = None
+            self.size -= 1
+            return right
+
+        node.left = self.removeMinimum(node.left)
+
+        return node
+
+    def removeMin(self, node: TreeNode) -> None:
+        if self.root:
+            self.root = self.removeMinimum(self.root)
