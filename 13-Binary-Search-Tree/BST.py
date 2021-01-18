36a298d40b24d9a66e0ee360e6a32f02199b7d86
cbqin <cbqin0323@gmail.com>
2021-01-18 11:46:52 +0800

    update bst

--- 13-Binary-Search-Tree/BST.py
+++ 13-Binary-Search-Tree/BST.py
@@ -103,3 +103,33 @@ class BST(object):
         if self.isEmpty():
             raise NoSuchElementError("Delete Max.", "BST is empty.")
         self.root.self._deleteMax(self.root)
+
+    def _delete(self, node: Node, key: int)->Node:
+        if node is None:
+            return None
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
+        else:
+            if node.left is None:
+                return node.right
+            if node.right is None:
+                return node.left
+            temp = node
+            node = self._min(temp.right)
+            node.right = self._deleteMin(temp.right)
+            node.left = temp.left
+        node.size = 1+self._size(node.left)+self._size(node.right)
+        return node
+
+    def delete(self, key: int)->None:
+        if self.isEmpty():
+            raise NoSuchElementError("Delete.", "BST is empty.")
+        self.root = self._delete(self.root, key)
+
+    def _min(self, node: Node)->None:
+        pass
+
+    def min(self)->Node:
+        pass
