62b7bbfb6d14b753eea534e32e3db990b1176db2
cbqin <cbqin0323@gmail.com>
2021-01-18 15:55:04 +0800

    update bst

--- 13-Binary-Search-Tree/02-Importance/230.二叉搜索树中第k小的元素.py
+++ 13-Binary-Search-Tree/02-Importance/230.二叉搜索树中第k小的元素.py
@@ -14,12 +14,12 @@
 # Testcase Example:  '[3,1,4,null,2]\n1'
 #
 # 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
-# 
+#
 # 说明：
 # 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
-# 
+#
 # 示例 1:
-# 
+#
 # 输入: root = [3,1,4,null,2], k = 1
 # ⁠  3
 # ⁠ / \
@@ -27,9 +27,9 @@
 # ⁠ \
 # 2
 # 输出: 1
-# 
+#
 # 示例 2:
-# 
+#
 # 输入: root = [5,3,6,2,4,null,null,1], k = 3
 # ⁠      5
 # ⁠     / \
@@ -39,10 +39,10 @@
 # ⁠ /
 # ⁠1
 # 输出: 3
-# 
+#
 # 进阶：
 # 如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
-# 
+#
 #
 
 # @lc code=start
@@ -52,8 +52,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution:
     def kthSmallest(self, root: TreeNode, k: int) -> int:
-        
-# @lc code=end
+        def dfs(root, res):
+            if not root:
+                return
+            dfs(root.left, res)
+            res.append(root.val)
+            dfs(root.right, res)
 
+        res = []
+        dfs(root, res)
+        return res[k-1]
+
+
+# @lc code=end
