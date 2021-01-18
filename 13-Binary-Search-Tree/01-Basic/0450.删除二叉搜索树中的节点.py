add622c3ecaf83e37272d0620e5cfd51f8510e1d
cbqin <cbqin0323@gmail.com>
2021-01-18 18:42:42 +0800

    update bst

---
+++ 13-Binary-Search-Tree/01-Basic/0450.删除二叉搜索树中的节点.py
@@ -0,0 +1,102 @@
+#
+# @lc app=leetcode.cn id=450 lang=python3
+#
+# [450] 删除二叉搜索树中的节点
+#
+# https://leetcode-cn.com/problems/delete-node-in-a-bst/description/
+#
+# algorithms
+# Medium (45.18%)
+# Likes:    377
+# Dislikes: 0
+# Total Accepted:    30.3K
+# Total Submissions: 66.5K
+# Testcase Example:  '[5,3,6,2,4,null,7]\n3'
+#
+# 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key
+# 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。
+#
+# 一般来说，删除节点可分为两个步骤：
+#
+#
+# 首先找到需要删除的节点；
+# 如果找到了，删除它。
+#
+#
+# 说明： 要求算法时间复杂度为 O(h)，h 为树的高度。
+#
+# 示例:
+#
+#
+# root = [5,3,6,2,4,null,7]
+# key = 3
+#
+# ⁠   5
+# ⁠  / \
+# ⁠ 3   6
+# ⁠/ \   \
+# 2   4   7
+#
+# 给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+#
+# 一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+#
+# ⁠   5
+# ⁠  / \
+# ⁠ 4   6
+# ⁠/     \
+# 2       7
+#
+# 另一个正确答案是 [5,2,6,null,4,null,7]。
+#
+# ⁠   5
+# ⁠  / \
+# ⁠ 2   6
+# ⁠  \   \
+# ⁠   4   7
+#
+#
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        def min(root):
+            if not root.left:
+                return root
+            return min(root.left)
+
+        def deleteMin(root):
+            if not root.left:
+                return root.right
+            root.left = deleteMin(root.left)
+            return root
+
+        def delete(root, key):
+            if not root:
+                return None
+            if key < root.val:
+                root.left = delete(root.left, key)
+            elif key > root.val:
+                root.right = delete(root.right, key)
+            else:
+                if root.left is None:
+                    return root.right
+                if root.right is None:
+                    return root.left
+                temp = root
+                root = min(temp.right)
+                root.right = deleteMin(temp.right)
+                root.left = temp.left
+            return root
+
+        return delete(root, key)
+# @lc code=end
